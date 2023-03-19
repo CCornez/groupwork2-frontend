bb82be4a01ee5b532f4ecb73eacd3613d1b0d81a
Noah Bovee <noahbov@gmail.com>
2023-03-19 10:46:55 +0100

    finished note-component

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,25 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import Note from "./Note";
 
-const CreateArea = (props) => {
+const CreateArea = ({ onAdd }) => {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setNote((preValue) => {
+      return {
+        ...preValue,
+        [name]: value,
+      };
+    });
+  }
+
+  const submitButton = (event) => {
+    onAdd(note);
+    setNote({
+      title: "",
+      content: "",
+    });
+    event.preventDefault();
+  };
   return (
     <>
-      <section className="section has-text-centerd">
+      <section className="section">
         <div className="container">
           <div className="columns is-centered">
-            <form action="" className="box " id="title-box">
+            <form className="box" id="title-box">
               <input
+                name="title"
+                onChange={handleChange}
                 className="my-1 mx-1 mb-2 is-size-5 has-text-weight-bold"
+                value={note.title}
                 type="text"
                 placeholder="Title"
               />
               <p>
                 <textarea
                   className="my-1 mx-1 is-size-6"
+                  value={note.content}
                   name="content"
-                  id=""
                   placeholder="Take a note..."
-                ></textarea>
+                  onChange={handleChange}
+                >
+                  {" "}
+                </textarea>
               </p>
+              <button
+                className="button is-warning is-rounded"
+                type="submit"
+                onClick={submitButton}
+              >
+                +
+              </button>
             </form>
           </div>
         </div>
