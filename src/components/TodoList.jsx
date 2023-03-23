53ad24b5d5b1be2e3b6c8107a548ed6d2d421fec
Gaspar Saldana Sandoval <gasparsaldanas@Gaspars-MacBook-Air.local>
2023-03-23 23:15:34 +0100

    post go to database but not showing in the list

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,26 +1,11 @@
-import React, { useState } from "react";
-import { nanoid } from "nanoid";
+import React from "react";
 
-
-const TodoList = () => {
-  const [todoLists, setTodoLists] = useState([]);
-  const [newTodoListName, setNewTodoListName] = useState("");
-
-  const handleAddTodoList = (e) => {
-    e.preventDefault();
-    if (newTodoListName) {
-      setTodoLists((prevTodoLists) => [
-        ...prevTodoLists,
-        { id: nanoid(), name: newTodoListName, todos: [] },
-      ]);
-      setNewTodoListName("");
-    }
-  };
-
-  const handleTodoListNameChange = (e) => {
-    setNewTodoListName(e.target.value);
-  };
-
+const TodoList = ({
+  todoLists,
+  handleAddTodoList,
+  newTodoListName,
+  handleTodoListNameChange,
+}) => {
   return (
     <div className="container">
       <form onSubmit={handleAddTodoList}>
@@ -42,24 +27,27 @@ const TodoList = () => {
         </div>
       </form>
       <div className="columns is-multiline">
-        {todoLists.map((todoList) => (
-          <div className="column is-one-quarter" key={todoList.id}>
-            <div className="card">
-              <div className="card-header">
-                <p className="card-header-title">{todoList.name}</p>
-              </div>
-              <div className="card-content">
-                <div className="content">
-                  <ul>
-                    {todoList.todos.map((todo) => (
-                      <li key={todo.id}>{todo.text}</li>
-                    ))}
-                  </ul>
+        {todoLists &&
+          todoLists.length > 0 &&
+          todoLists.map((todoList) => (
+            <div className="column is-one-quarter" key={todoList.id}>
+              <div className="card">
+                <div className="card-header">
+                  <p className="card-header-title">{todoList.title}</p>
+                </div>
+                <div className="card-content">
+                  <div className="content">
+                    <ul>
+                      {todoList.todos &&
+                        todoList.todos.map((todo) => (
+                          <li key={todo.id}>{todo.text}</li>
+                        ))}
+                    </ul>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );
